0f092665c6ef7885d2b35dacffd851b5c4cf57ce
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-23 16:31:27 +0900

    #3.1 Introduction to the User Model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- users/models.py
+++ users/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class User(AbstractUser):
 
-    pass
+    bio = models.TextField(default="")
