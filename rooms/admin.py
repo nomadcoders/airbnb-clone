2c41dc0e3f3b388ea7938d44de58edd9ba741e43
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-23 18:24:15 +0900

    #4.3 ManyToMany like a Boss

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,24 @@ from core import models as core_models
 from users import models as user_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
@@ -22,3 +40,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    def __str__(self):
+        return self.name
