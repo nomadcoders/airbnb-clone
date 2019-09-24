d8313807614f4d0d6b9a80f9fa0cbe5ce35395c7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-24 16:07:59 +0900

    #6.0 Room Admin Panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,23 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("instant_book", "city", "country")
+
+    search_fields = ("=city", "^host__username")
 
 
 @admin.register(models.Photo)
