eacdcd714ebdfc27c0872645c880d3a9a6fd818f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-25 17:51:33 +0900

    #8.8 Intercepting Model save() and admin_save()

--- rooms/models.py
+++ rooms/models.py
@@ -92,6 +92,10 @@ class Room(core_models.TimeStampedModel):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        self.city = str.capitalize(self.city)
+        super().save(*args, **kwargs)
+
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
