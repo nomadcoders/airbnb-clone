dbca83d032834b4bab9958e5f95ed297fcb77437
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-26 16:13:38 +0900

    #9.7 seed_lists

---
+++ lists/management/commands/seed_list.py
@@ -0,0 +1,38 @@
+import random
+from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
+from django_seed import Seed
+from lists import models as list_models
+from users import models as user_models
+from rooms import models as room_models
+
+
+NAME = "lists"
+
+
+class Command(BaseCommand):
+
+    help = f"This command creates {NAME}"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=2, type=int, help=f"How many {NAME} you want to create"
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder.add_entity(
+            list_models.List, number, {"user": lambda x: random.choice(users)}
+        )
+
+        created = seeder.execute()
+        cleaned = flatten(list(created.values()))
+        for pk in cleaned:
+            list_model = list_models.List.objects.get(pk=pk)
+            to_add = rooms[random.randint(0, 5) : random.randint(6, 30)]
+            list_model.rooms.add(*to_add)
+
+        self.stdout.write(self.style.SUCCESS(f"{number} {NAME} created!"))
--- rooms/models.py
+++ rooms/models.py
@@ -102,5 +102,5 @@ class Room(core_models.TimeStampedModel):
         if len(all_reviews) > 0:
             for review in all_reviews:
                 all_ratings += review.rating_average()
-            return all_ratings / len(all_reviews)
+            return round(all_ratings / len(all_reviews), 2)
         return 0
