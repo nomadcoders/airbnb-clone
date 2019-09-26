af92e8a50184e48e40d4a33f9d0ce7bd1c6cae3d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-26 16:48:07 +0900

    #9.8 seed_reservations

--- reservations/management/commands/seed_reservations.py
+++ reservations/management/commands/seed_reservations.py
@@ -1,13 +1,13 @@
 import random
+from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand
-from django.contrib.admin.utils import flatten
 from django_seed import Seed
-from lists import models as list_models
+from reservations import models as reservation_models
 from users import models as user_models
 from rooms import models as room_models
 
 
-NAME = "lists"
+NAME = "reservations"
 
 
 class Command(BaseCommand):
@@ -25,14 +25,18 @@ class Command(BaseCommand):
         users = user_models.User.objects.all()
         rooms = room_models.Room.objects.all()
         seeder.add_entity(
-            list_models.List, number, {"user": lambda x: random.choice(users)}
+            reservation_models.Reservation,
+            number,
+            {
+                "status": lambda x: random.choice(["pending", "confirmed", "canceled"]),
+                "guest": lambda x: random.choice(users),
+                "room": lambda x: random.choice(rooms),
+                "check_in": lambda x: datetime.now(),
+                "check_out": lambda x: datetime.now()
+                + timedelta(days=random.randint(3, 25)),
+            },
         )
 
-        created = seeder.execute()
-        cleaned = flatten(list(created.values()))
-        for pk in cleaned:
-            list_model = list_models.List.objects.get(pk=pk)
-            to_add = rooms[random.randint(0, 5) : random.randint(6, 30)]
-            list_model.rooms.add(*to_add)
+        seeder.execute()
 
         self.stdout.write(self.style.SUCCESS(f"{number} {NAME} created!"))
--- reservations/models.py
+++ reservations/models.py
@@ -34,7 +34,7 @@ class Reservation(core_models.TimeStampedModel):
 
     def in_progress(self):
         now = timezone.now().date()
-        return now > self.check_in and now < self.check_out
+        return now >= self.check_in and now <= self.check_out
 
     in_progress.boolean = True
 
