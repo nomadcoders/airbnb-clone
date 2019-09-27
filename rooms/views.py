415b6016c9b0b48e9b203e88cd2cd0d392e944b4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-27 15:27:06 +0900

    #11.5 get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -5,8 +5,8 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.page(int(page))
+    return render(request, "rooms/home.html", {"page": rooms})
