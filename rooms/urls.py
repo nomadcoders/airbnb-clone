5cce822e40876a255c870b1062996c9f1a859efa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-28 14:36:28 +0900

    #13.0 Template, Form, Url, Setup

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "rooms"
 
-urlpatterns = [path("<int:pk>", views.RoomDetail.as_view(), name="detail")]
+urlpatterns = [
+    path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
+    path("search/", views.search, name="search"),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -18,3 +19,9 @@ class RoomDetail(DetailView):
     """ RoomDetail Definition """
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
