1d811ad8501c196abf8ad015c540602061c8d08c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-28 16:10:56 +0900

    #13.5 Filering Like a Boss part One

--- rooms/views.py
+++ rooms/views.py
@@ -63,4 +63,16 @@ def search(request):
         "facilities": facilities,
     }
 
-    return render(request, "rooms/search.html", {**form, **choices})
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
