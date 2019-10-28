0c134eb6d53d21cbb998be628420eb85096cb0b6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-28 17:19:40 +0900

    #14.4 Log In and Log Out

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
@@ -11,5 +12,15 @@ class LoginView(View):
     def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
         return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
