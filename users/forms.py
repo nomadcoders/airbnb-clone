01d97b6955b7bd4755ff963db9a6f353e42d00ae
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-28 16:52:22 +0900

    #14.3 Validating Password

--- users/forms.py
+++ users/forms.py
@@ -7,13 +7,14 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
         try:
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-
-    def clean_password(self):
-        return "lalalalalalal"
+            self.add_error("email", forms.ValidationError("User does not exist"))
