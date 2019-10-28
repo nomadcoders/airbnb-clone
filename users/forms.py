1de3c9c9bc6cda9253c1d97dfcc1607463c35fc0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-28 16:13:58 +0900

    #14.2 Validating Email

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(username=email)
+            return email
+        except models.User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        return "lalalalalalal"
--- users/views.py
+++ users/views.py
@@ -5,9 +5,11 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "itn@las.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            print(form.cleaned_data)
+        return render(request, "users/login.html", {"form": form})
