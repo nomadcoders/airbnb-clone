dfa23bc871815a69d3e5078de25ecc6616b1f7d0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-28 15:32:42 +0900

    #14.0 Setting Up

---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        pass
