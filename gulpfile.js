0fdd97d3141e10631dbeba93a4770e5b86664cb0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-30 16:53:21 +0900

    #19.1 Setting Up TailwindCSS with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+
+const css = () => {
+  const postCSS = require("gulp-postcss");
+  const sass = require("gulp-sass");
+  const minify = require("gulp-csso");
+  sass.compiler = require("node-sass");
+  return gulp
+    .src("assets/scss/styles.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postCSS([require("tailwindcss"), require("autoprefixer")]))
+    .pipe(minify())
+    .pipe(gulp.dest("static/css"));
+};
+
+exports.default = css;
--- users/forms.py
+++ users/forms.py
@@ -22,5 +22,25 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(UserCreationForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
 
-    username = forms.EmailField(label="Email")
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
