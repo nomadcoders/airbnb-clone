6fa859525b40505ca1a500e7d141043e219da190
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-10-31 16:20:29 +0900

    #20.6 Room Cards part Two

--- rooms/views.py
+++ rooms/views.py
@@ -9,7 +9,7 @@ class HomeView(ListView):
     """ HomeView Definition """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
