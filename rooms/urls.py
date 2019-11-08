727dd9bdde8cf188de71d39425b142d03c140161
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-11-08 16:11:10 +0900

    #23.2 Room Photos part Two

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,6 +6,6 @@ app_name = "rooms"
 urlpatterns = [
     path("<int:pk>/", views.RoomDetail.as_view(), name="detail"),
     path("<int:pk>/edit/", views.EditRoomView.as_view(), name="edit"),
-    path("<int:pk>/photos/", views.EditRoomView.as_view(), name="photos"),
+    path("<int:pk>/photos/", views.RoomPhotosView.as_view(), name="photos"),
     path("search/", views.SearchView.as_view(), name="search"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -140,11 +140,10 @@ class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
 class RoomPhotosView(user_mixins.LoggedInOnlyView, DetailView):
 
     model = models.Room
-    template_name = "room_photos.html"
+    template_name = "rooms/room_photos.html"
 
     def get_object(self, queryset=None):
         room = super().get_object(queryset=queryset)
         if room.host.pk != self.request.user.pk:
             raise Http404()
         return room
-
