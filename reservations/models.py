6f7996fc9251598ba5c52df951587c866ce8a6ca
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-11-14 18:01:15 +0900

    #24.6 BookedDays part Two

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
@@ -55,12 +56,17 @@ class Reservation(core_models.TimeStampedModel):
     is_finished.boolean = True
 
     def save(self, *args, **kwargs):
-        if True:
+        if self.pk is None:
             start = self.check_in
             end = self.check_out
             difference = end - start
             existing_booked_day = BookedDay.objects.filter(
                 day__range=(start, end)
             ).exists()
-
+            if not existing_booked_day:
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                return
         return super().save(*args, **kwargs)
