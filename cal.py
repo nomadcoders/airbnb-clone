97088cf2cb4816fc902806a1613f6f6136387a87
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-11-14 16:49:51 +0900

    #24.1 Room Calendar part Two

--- cal.py
+++ cal.py
@@ -1,11 +1,12 @@
 import calendar
 
 
-class Calendar:
+class Calendar(calendar.Calendar):
     def __init__(self, year, month):
+        super().__init__(firstweekday=6)
         self.year = year
         self.month = month
-        self.day_names = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+        self.day_names = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
         self.months = (
             "January",
             "February",
@@ -20,6 +21,14 @@ class Calendar:
             "November",
             "December",
         )
+
+    def get_days(self):
+        weeks = self.monthdays2calendar(self.year, self.month)
+        days = []
+        for week in weeks:
+            for day, _ in week:
+                days.append(day)
+        return days
 
     def get_month(self):
         return self.months[self.month - 1]
--- rooms/models.py
+++ rooms/models.py
@@ -122,6 +122,6 @@ class Room(core_models.TimeStampedModel):
         return photos
 
     def get_calendars(self):
-        calendar = Calendar(2019, 11)
-        print(calendar.get_month())
-        return False
+        this_month = Calendar(2019, 11)
+        next_month = Calendar(2019, 12)
+        return [this_month, next_month]
