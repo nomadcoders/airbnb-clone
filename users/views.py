7e249dc443fd10ead0c91a327c08351a95c64f03
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-11-19 16:58:44 +0900

    #25.2 Changing Language

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.utils import translation
 from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
@@ -272,5 +273,5 @@ def switch_hosting(request):
 def switch_language(request):
     lang = request.GET.get("lang", None)
     if lang is not None:
-        pass
+        request.session[translation.LANGUAGE_SESSION_KEY] = lang
     return HttpResponse(status=200)
