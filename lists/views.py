f4d734c4c82d2265f1e09dc5a464faa9b500c303
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-12-17 22:51:19 +0900

    toggle_room login_required

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, reverse
 from django.views.generic import TemplateView
 from rooms import models as room_models
 from . import models
 
 
+@login_required
 def toggle_room(request, room_pk):
     action = request.GET.get("action", None)
     room = room_models.Room.objects.get_or_none(pk=room_pk)
