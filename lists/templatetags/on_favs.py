54781e2f0df227aa3efdb561bd93cf1660ea3bb1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-12-17 18:23:50 +0900

    on_favs

--- lists/templatetags/on_favs.py
+++ lists/templatetags/on_favs.py
@@ -7,5 +7,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def on_favs(context, room):
     user = context.request.user
-    the_list = list_models.List.objects.get_or_none(user=user)
-    return room in the_list.rooms.all()
+    if user.is_authenticated:
+        the_list = list_models.List.objects.get_or_none(user=user)
+        if the_list is not None:
+            return room in the_list.rooms.all()
+        else:
+            return False
+    else:
+        return False
